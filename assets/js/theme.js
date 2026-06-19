0fcc2912f0527ca74748266303ee500c1e1c5bdf
oauth-network <127634415+oauth-network@users.noreply.github.com>
2026-06-20 02:56:04 +0900

    ✨🐛 레이아웃 얼라이먼트
    - 메인페이지 레이아웃 조정
    - 상세페이지 레이아웃 조정

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,16 +1,79 @@
 (function ($) {"use strict";
 
 $(function () {
-  var header = $(".start-style");
-  $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
+  var $window = $(window);
 
-    if (scroll >= 10) {
+  var header = $(".start-style");
+
+  var $sidebar = $(".sidebar");
+  var $article = $(".mainheading");
+
+  var $endScroll = $('.post-top-meta');
+
+  var $toc = $('.toc-content #markdown-toc');
+
+  var scrollTop = $('html,body').scrollTop();
+
+  if($article.length){
+    var article_top = $article.offset().top;
+
+    if(scrollTop >= article_top){
+      $sidebar.addClass("sticky")
+    }else{
+      $sidebar.removeClass("sticky")
+    }
+
+    var windowHeight = $window.height();
+    var marginTop = windowHeight < $toc.height() ? ($toc.height() - windowHeight) + 90 : 0
+    var article_top = $article.offset().top + marginTop;
+    var article_end = $endScroll.offset().top - windowHeight - marginTop;
+
+    if(scrollTop >= article_end){
+      $sidebar.css("top",article_end - $window.height())
+      $sidebar.removeClass("sticky")
+    }else if(scrollTop >= article_top){
+      $sidebar.removeAttr("style")
+      $sidebar.addClass("sticky")
+    }
+  }
+
+  $window.scroll(function () {
+    var scrollTop = $('html,body').scrollTop();
+
+    console.log('scrollTop',scrollTop);
+
+    if (scrollTop > 0) {
       header.removeClass('start-style').addClass("scroll-on");
     } else {
       header.removeClass("scroll-on").addClass('start-style');
     }
-  });
+
+
+    if($article.length){
+      var windowHeight = $window.height();
+      var marginTop = windowHeight < $toc.height() ? ($toc.height() - windowHeight) + 90 : 0
+      var article_top = $article.offset().top;
+      var article_end = $endScroll.offset().top - windowHeight - marginTop;
+
+      if(scrollTop >= article_end){
+        $sidebar.css("top",article_end - $window.height())
+        $sidebar.removeClass("sticky")
+        return
+      }else{
+        $sidebar.removeAttr("style")
+        $sidebar.addClass("sticky")
+      }
+
+      if(scrollTop >= article_top){
+          $sidebar.addClass("sticky")
+        }else{
+          $sidebar.removeClass("sticky")
+        }
+      }
+
+    });
+
+    $('.article-post table').wrap('<div class="table-container"></div>');
 });
 
 //Animation
@@ -34,5 +97,10 @@ $('body').on('mouseenter mouseleave', '.nav-item', function (e) {
 $(document.links).filter(function() {
 return this.hostname != window.location.hostname;
 }).attr('target', '_blank');
+
+// Sidebar Toggle
+$('.sidebar-toggle-btn').on('click', function() {
+  $('.sidebar-wrapper').toggleClass('show');
+});
 
 })(jQuery);
